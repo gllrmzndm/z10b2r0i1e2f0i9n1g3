c33c7eff342f1deff4f3293d826d2149db3bdb43
Thisisguillermo <guillermozaandam@hotmail.com>
2022-07-05 21:45:32 +0200

    hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,11 +2,16 @@ import * as React from "react"
 import { Link, useStaticQuery, graphql } from 'gatsby'
 import { StaticImage } from "gatsby-plugin-image"
 
+let name = "Guillermo Zaandam"
+
 
 const Hero = () => {
     return (
-        <div>
-            <StaticImage src="../images/fade2.jpg" alt="Image of the agent Fade from Valorant" />
+        <div className="grid grid-cols-1 relative">
+            <div className="z-0">
+                <StaticImage src="../images/guillermo.png" alt="Image of the agent Fade from Valorant" layout="fullWidth" />
+            </div>
+                <p className="z-10 absolute inset-0 flex items-center justify-center text-gray-50 text-xl">Hello I am {name}</p>
         </div>
     )
 }
