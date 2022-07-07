d6e761eece2de20a29a58e39f0650ccc1382f23d
Thisisguillermo <guillermozaandam@hotmail.com>
2022-07-07 17:12:28 +0200

    better nav

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,18 +1,24 @@
 import * as React from "react"
 import { Link, useStaticQuery, graphql } from 'gatsby'
 import { StaticImage } from "gatsby-plugin-image"
+import ReactPlayer from 'react-player/youtube'
 
 let name = "Guillermo Zaandam"
 
 
 const Hero = () => {
     return (
-        <div className="grid grid-cols-1 relative">
-            <div className="z-0">
-                <StaticImage src="../images/guillermo.png" alt="Image of the agent Fade from Valorant" layout="fullWidth" />
+        <header className="grid grid-cols-1 relative">
+            <div className="z-0 border-2 border-red-400">
+                <StaticImage src="../images/guillermo.png" alt="Image of the agent Fade from Valorant" layout="fullWidth" placeholder="dominantcolor" />
+                <p className="z-10 absolute inset-0 flex items-center justify-center text-gray-100 body-font font-ubuntu text-2xl">Hello I am {name}</p>
             </div>
-                <p className="z-10 absolute inset-0 flex items-center justify-center text-gray-50 text-xl">Hello I am {name}</p>
-        </div>
+
+            {/* <div className="player-wrapper pointer-events-none w-full">
+                <ReactPlayer className='object-cover aspect-video' preload='true' playing='true' muted='true' loop='true' playsinline='true' width="100%"
+                    height="100%" frameborder="0" url='https://youtu.be/7Xz8UdGFJDs' />
+            </div> */}
+        </header>
     )
 }
 
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
     return (
         <div className="w-full">
             <nav className="w-full">
-                <ul className="hidden sm:grid grid-rows-4 grid-flow-col gap-4 lg:flex items-center justify-center space-x-48 mx-28">
-                    <li className="py-2 text-black hover:bg-rose-600 hover:text-white"><Link to="/">Guillermo Zaandam</Link></li>
+                <ul className="hidden sm:flex items-center lg:justify-between space-x-10 mx-20">
+                    <li className="py-2 text-black hover:bg-orange-600 hover:text-white"><Link to="/">Guillermo Zaandam</Link></li>
                     <li className="py-2 "><Link to="/">About me</Link></li>
                     <li className="py-2 "><Link to="/">Articles</Link></li>
                     <li className="py-2 "><Link to="/">Projects</Link></li>
@@ -14,9 +14,9 @@ const Navbar = () => {
 
                 <ul className="sm:hidden bg-gray-200 h-10 border-solid border-2 border-rose-600">
                     <div className="space-y-2 py-2 px-2">
-                        <span className="block h-0.5 w-8 bg-rose-600"></span>
-                        <span className="block h-0.5 w-8 bg-rose-600"></span>
-                        <span className="block h-0.5 w-8 bg-rose-600"></span>
+                        <span className="block h-0.5 w-8 bg-gray-800"></span>
+                        <span className="block h-0.5 w-8 bg-gray-800"></span>
+                        <span className="block h-0.5 w-8 bg-gray-800"></span>
                     </div>
                 </ul>
             </nav>
